1685eaf1f640b3fbc09184044fddfa56537b953f
22f3002094 <22f3002094@ds.study.iitm.ac.in>
2025-05-12 11:08:00 +0530

    Milestone-0 VP-MAD-1

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from Backend.models import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.sqlite3"
+    db.init_app(app)
+    
+    app.app_context().push()
+    return app
+
+app = create_app()
+
+from Backend.routes import *
+
+if __name__ == "__main__":
+    app.run()
